0d4707139b14ee61029a44ffa6857a5f59b8513c
Sivert Høilund Sivertsen <45416991+mrhandsomecat@users.noreply.github.com>
2023-03-21 14:46:24 +0100

    Data cleanup + miniscule c-code

--- software/angle_calc/angle_calc.c
+++ software/angle_calc/angle_calc.c
@@ -1,4 +1,29 @@
+#include <math.h>
 
+
+#define C 343.3  // Lydhastighet 
+#define a 0.075  // Sidelengde tetraheder
+
+// The vectors between each point in the tetrahedral
+// Potential Todo: Just define this as a matrix directly 
+float r21[3] = {-0.5*a, math.sqrt(3)*a/2, 0};
+float r31[3] = {-a, 0, 0};
+float r41[3] = {-0.5*a, a*math.sqrt(3)/6, math.sqrt(2*a/3)};
+float r32[3] = {-0.5*a, -math.sqrt(3)*a/2, 0};
+float r42[3] = {0, -math.sqrt(3)*a/3, math.sqrt(2*a/3)};
+float r43[3] = {0.5*a, math.sqrt(3)*a/6, math.sqrt(2*a/3)};
+
+
+float[][] transpose(int row, int column, float matrix[][]){
+    float transpose_matrix[][];
+    for (int i = 0; i < row; i++){
+        for (int j = 0; j < column; j++){
+            transpose_matrix[j][i] = matrix[i][j];
+        }
+    }
+    return transpose_matrix[][];
+     
+}
 
 int least_square(){
     return 0;
