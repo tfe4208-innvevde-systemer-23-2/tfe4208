bcb1902a6c75163b598d09b4198e059d4d724f15
Mathias Støle <mathst23@gmail.com>
2023-03-23 11:50:07 +0100

    Initial commit of Peak IP

---
+++ ip/Peak/rtl/Peak.sv
@@ -0,0 +1,14 @@
+// Copyright
+
+module Peak #(
+    parameter MAX_LAGS                  = 17;
+    parameter NUM_XCORRS                = 6;
+) (
+    input  logic                                    clk;
+    input  logic                                    rst;
+    
+);
+
+// Need edge detector???
+
+endmodule
---
+++ ip/Peak/sim/Peak_test.sv
@@ -0,0 +1,5 @@
+// Copyright
+
+module Peak_test;
+
+endmodule;
