d383eb4af684232160a79660c3aabfb30f3fd0f6
Mathias Støle <mathst23@gmail.com>
2023-03-27 23:18:30 +0200

    Add some resembelnse of a testbench

--- ip/CpuPeripheral/sim/CpuPeripheral_test.sv
+++ ip/CpuPeripheral/sim/CpuPeripheral_test.sv
@@ -1,5 +1,31 @@
 // Copyright
 
 module CpuPeripheral_test;
+    // -- Parameters --
+    parameter NUM_SLAVES = 4,                // Number of SPI channels
+    parameter MAX_LAGS = 17,                 // Maximum Correlation delay in num of samples (has to be < 32)
+    parameter CPU_BITS = 32                  // Number of bits used for CPU communication
 
-endmodule;
+    // DUT
+    CpuPeripheral #(
+        .NUM_SLAVES     (NUM_SLAVES),
+        .MAX_LAGS       (MAX_LAGS),
+        .CPU_BITS       (CPU_BITS)
+    ) dut (
+        .clk            (),
+        .reset          (),
+        .readdata       (),
+        .dataIn         (),
+        .dataInValid    ()
+    );
+
+    initial begin
+        clk50M = 1'b0;
+        //stim;
+        //@(posedge clk50M); $finish(2);
+    end
+
+    // Generate clock
+    always #10 clk50M=!clk50M;
+
+endmodule
