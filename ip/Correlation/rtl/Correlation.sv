5f605e4fc76fd35c6eb442d8103c85cb2a2c718a
TFE4171Spring2023 Group 9 <dds223s09@aurora.tele.ntnu.no>
2023-03-31 15:03:24 +0200

    Correct syntax errors

--- ip/Correlation/rtl/Correlation.sv
+++ ip/Correlation/rtl/Correlation.sv
@@ -6,35 +6,37 @@
 ////////////////////////////////////////////////////
 
 module Correlation #(
-    parameter NUM_BITS_SAMPLE           = 12;
-    parameter NUM_SLAVES                = 4;
-    parameter NUM_SAMPLES               = 100;
-    parameter MAX_SAMPLES_DELAY         = 11;
-    parameter NUM_BITS_XCORR            = 2 * NUM_BITS_SAMPLE + $clog2(NUM_SAMPLES);
-    parameter NUM_XCORRS                = 6;
+    parameter NUM_BITS_SAMPLE           = 12,
+    parameter NUM_SLAVES                = 4,
+    parameter NUM_SAMPLES               = 100,
+    parameter MAX_SAMPLES_DELAY         = 11,
+    parameter NUM_BITS_XCORR            = 2 * NUM_BITS_SAMPLE + $clog2(NUM_SAMPLES),
+    parameter NUM_XCORRS                = 6
 ) (
-    input  logic                                            clk;
-    input  logic                                            rst;
-    input  logic[NUM_SLAVES-1:0][NUM_BITS_SAMPLE-1:0]       dataIn;
+    input  logic                                            clk,
+    input  logic                                            rst,
+    input  logic[NUM_SLAVES-1:0][NUM_BITS_SAMPLE-1:0]       dataIn,
+
+    output logic[NUM_XCORRS-1:0][2*MAX_SAMPLES_DELAY:0][NUM_BITS_XCORR-1:0] xCorrOut
 );
 
 // Need edge detector???
 
 // Shift register for storing the input data
-reg inputBuffer [NUM_SLAVES-1:0][NUM_SAMPLES-1:0][NUM_BITS_SAMPLE-1:0];
+logic [NUM_SLAVES-1:0][NUM_SAMPLES-1:0][NUM_BITS_SAMPLE-1:0] inputBuffer;
 
 // Values used for crosscorrelation calculations. The f and g refer to the symbols in the crosscorrelation formula.
 // The add-values are used when introducing a new value to crosscorrelation estimate, and the sub-values are used when they are removed.
-logic xCorrInputAddf [NUM_SLAVES-1:0][NUM_BITS_SAMPLE-1:0];
-logic xCorrInputSubf [NUM_SLAVES-1:0][NUM_BITS_SAMPLE-1:0];
-logic xCorrInputAddg [NUM_SLAVES-1:0][MAX_SAMPLES_DELAY:0][NUM_BITS_SAMPLE-1:0];
-logic xCorrInputSubg [NUM_SLAVES-1:0][MAX_SAMPLES_DELAY:0][NUM_BITS_SAMPLE-1:0];
+logic [NUM_SLAVES-1:0][NUM_BITS_SAMPLE-1:0] xCorrInputAddf;
+logic [NUM_SLAVES-1:0][NUM_BITS_SAMPLE-1:0] xCorrInputSubf;
+logic [NUM_SLAVES-1:0][MAX_SAMPLES_DELAY:0][NUM_BITS_SAMPLE-1:0] xCorrInputAddg;
+logic [NUM_SLAVES-1:0][MAX_SAMPLES_DELAY:0][NUM_BITS_SAMPLE-1:0] xCorrInputSubg;
 
 // Registers for storing the crosscorrelation values
-reg xCorr [NUM_XCORRS-1:0][2*MAX_SAMPLES_DELAY:0][NUM_BITS_XCORR-1:0];
+logic [NUM_XCORRS-1:0][2*MAX_SAMPLES_DELAY:0][NUM_BITS_XCORR-1:0] xCorr;
 
 // Eunmeration for the different crosscorrelations
-typedef enum logic [2:0] {xCorr01 = 0, xCorr02 = 1, xCorr03 = 2, xCorr12 = 3, xCorr13 = 4, xCorr23 = 5} xCorrIndex;
+typedef enum logic [2:0] {xCorr01 = 0, xCorr02 = 1, xCorr03 = 2, xCorr12 = 3, xCorr13 = 4, xCorr23 = 5} xCorrIndex_t;
 
 genvar slave, bufferLine;
 generate
@@ -43,14 +45,15 @@ generate
         // Sets up the inputBuffer shift register
         always @(posedge clk) begin
             if (rst) begin
-                inputBuffer[slave] <= {NUM_SAMPLES{NUM_BITS_SAMPLE{1'b0}}};
+                inputBuffer[slave] <= '0;
             end else begin
                 // Shift in new sample
-                inputBuffer[slave][0] <= dataIn[slave];
-                // Shift all other samples down
-                for (bufferLine = 0; bufferLine <= NUM_SAMPLES; bufferLine = bufferLine + 1) begin
-                    inputBuffer[slave][bufferLine+1] <= inputBuffer[slave][bufferLine];
-                end
+                inputBuffer[slave] <= {dataIn[slave], inputBuffer[slave][NUM_SAMPLES-2:0]};
+                // inputBuffer[slave][0] <= dataIn[slave];
+                // // Shift all other samples down
+                // for (bufferLine = 0; bufferLine < NUM_SAMPLES; bufferLine = bufferLine + 1) begin
+                //     inputBuffer[slave][bufferLine+1] <= inputBuffer[slave][bufferLine];
+                // end
             end
         end
 
@@ -67,15 +70,15 @@ endgenerate
 
 // Instantiate the crosscorrelation iterators
 // Will set up calculations for xcorr(0,1), xcorr(0,2), xcorr(0,3), xcorr(1,2), xcorr(1,3) and xcorr(2,3)
-genvar xCorrIndex, fIndex, gIndex;
+genvar fIndex, gIndex;
 generate
-    xCorrIndex = 0;
+    // xCorrIndex = 0;
     for (fIndex = 0; fIndex < NUM_SLAVES; fIndex = fIndex + 1) begin
         for (gIndex = fIndex + 1; gIndex < NUM_SLAVES; gIndex = gIndex + 1) begin
             CrossorrelationIterator #(
-                .NUM_BITS           (NUM_BITS_SAMPLE)
-                .NUM_SAMPLES        (NUM_SAMPLES)
-                .MAX_SAMPLES_DELAY  (MAX_SAMPLES_DELAY)
+                .NUM_BITS_SAMPLE    (NUM_BITS_SAMPLE),
+                .NUM_SAMPLES        (NUM_SAMPLES),
+                .MAX_SAMPLES_DELAY  (MAX_SAMPLES_DELAY),
                 .NUM_BITS_XCORR     (NUM_BITS_XCORR)
             ) u_CrossorrelationIterator (
                 .clk                (clk),
@@ -84,9 +87,9 @@ generate
                 .xCorrInputSubf     (xCorrInputSubf[fIndex]),
                 .xCorrInputAddg     (xCorrInputAddg[gIndex]),
                 .xCorrInputSubg     (xCorrInputSubg[gIndex]),
-                .xCorr              (xCorr[xCorrIndex])
+                // Index is a dirty hack to assign in order 0,1,2,3,4,5
+                .xCorr              (xCorr[(fIndex == 0) ? gIndex-1 : ((fIndex == 1) ? gIndex+1 : gIndex+2)])
             );
-            xCorrIndex = xCorrIndex + 1;
         end
     end
 endgenerate
--- ip/Correlation/rtl/CrosscorrelationIterator.sv
+++ ip/Correlation/rtl/CrosscorrelationIterator.sv
@@ -6,36 +6,38 @@
 ////////////////////////////////////////////////////
 
 module CrossorrelationIterator #(
-    parameter NUM_BITS_SAMPLE           = 12;
-    parameter NUM_SAMPLES               = 100;
-    parameter MAX_SAMPLES_DELAY         = 11;
-    parameter NUM_BITS_XCORR            = 2 * NUM_BITS_SAMPLE + $clog2(NUM_SAMPLES);
+    parameter NUM_BITS_SAMPLE           = 12,
+    parameter NUM_SAMPLES               = 100,
+    parameter MAX_SAMPLES_DELAY         = 11,
+    parameter NUM_BITS_XCORR            = 2 * NUM_BITS_SAMPLE + $clog2(NUM_SAMPLES)
 ) (
-    input  logic clk;
-    input  logic rst;
+    input  logic clk,
+    input  logic rst,
 
     // Values used for crosscorrelation calculations. The f and g refer to the symbols in the crosscorrelation formula.
     // The add-values are used when introducing a new value to crosscorrelation estimate, and the sub-values are used when they are removed.
-    input logic xCorrInputAddf [NUM_BITS_SAMPLE-1:0];
-    input logic xCorrInputSubf [NUM_BITS_SAMPLE-1:0];
-    input logic xCorrInputAddg [MAX_SAMPLES_DELAY:0][NUM_BITS_SAMPLE-1:0];
-    input logic xCorrInputSubg [MAX_SAMPLES_DELAY:0][NUM_BITS_SAMPLE-1:0];
+    input logic [NUM_BITS_SAMPLE-1:0] xCorrInputAddf,
+    input logic [NUM_BITS_SAMPLE-1:0] xCorrInputSubf,
+    input logic [MAX_SAMPLES_DELAY:0][NUM_BITS_SAMPLE-1:0] xCorrInputAddg,
+    input logic [MAX_SAMPLES_DELAY:0][NUM_BITS_SAMPLE-1:0] xCorrInputSubg,
 
     // Has the crosscorrelation values
-    output logic xCorr [2*MAX_SAMPLES_DELAY:0][NUM_BITS_XCORR-1:0];
+    output logic [2*MAX_SAMPLES_DELAY:0][NUM_BITS_XCORR-1:0] xCorr 
 );
 
 genvar delay;
 generate 
-    // Crosscorrelation calculations
-    always_ff @(posedge clk) begin
-        if (rst) begin
-            xCorr[delay] <= {NUM_BITS_XCORR{1'b0}};
-        end else begin
-            // Add and subtract new samples to the crosscorrelation estimate
-            xCorr[delay] <= xCorr[delay] 
-                            + xCorrInputAddf * xCorrInputAddg[delay]
-                            - xCorrInputSubf * xCorrInputSubg[delay];
+    for (delay = 0; delay < 2*MAX_SAMPLES_DELAY+1; delay = delay + 1) begin
+        // Crosscorrelation calculations
+        always_ff @(posedge clk) begin
+            if (rst) begin
+                xCorr[delay] <= '0;
+            end else begin
+                // Add and subtract new samples to the crosscorrelation estimate
+                xCorr[delay] <= xCorr[delay] 
+                                + xCorrInputAddf * xCorrInputAddg[delay]
+                                - xCorrInputSubf * xCorrInputSubg[delay];
+            end
         end
     end
 endgenerate
