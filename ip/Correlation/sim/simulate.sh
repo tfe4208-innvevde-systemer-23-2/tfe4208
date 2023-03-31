d7023824205724356dbb237c6856e621ccabb392
TFE4171Spring2023 Group 9 <dds223s09@aurora.tele.ntnu.no>
2023-03-31 15:02:04 +0200

    Add files to simulate script

--- ip/Correlation/sim/simulate.sh
+++ ip/Correlation/sim/simulate.sh
@@ -1,4 +1,4 @@
 rm -rf work
 vlib work
-vlog +cover=bcefsx -sv  Correlation_test.sv
+vlog +cover=bcefsx -sv ../rtl/Correlation.sv ../rtl/CrosscorrelationIterator.sv Correlation_test.sv
 vsim -c -coverage Correlation_test -do "run 2000ns;exit"
