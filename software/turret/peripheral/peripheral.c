7a4f3cc28cea1a294f65cddccb3a82c3a9b0c0d0
Daniel Vorhaug <dvorhaug@gmail.com>
2023-04-24 12:07:48 +0200

    Bugfix

--- software/turret/peripheral/peripheral.c
+++ software/turret/peripheral/peripheral.c
@@ -22,19 +22,19 @@ uint32_t peripheral_read(peripheral_dev *dev, uint8_t address) {
 }
 
 uint32_t peripheral_read_upper(peripheral_dev *dev) {
-	return peripheral_read(dev->base, PERIPHERAL_ADDR_UPPER);
+	return peripheral_read(dev, PERIPHERAL_ADDR_UPPER);
 }
 
 uint32_t peripheral_read_lower(peripheral_dev *dev) {
-	return peripheral_read(dev->base, PERIPHERAL_ADDR_LOWER);
+	return peripheral_read(dev, PERIPHERAL_ADDR_LOWER);
 }
 
 void getLags(peripheral_dev *dev, peripheral_lags *lags) {
-	uint32_t temp = peripheral_read_lower(dev->base);
+	uint32_t temp = peripheral_read_lower(dev);
 	lags->t01 = (temp & PERIPHERAL_MASK_FIRST);
 	lags->t02 = (temp & PERIPHERAL_MASK_SECOND) >> 6;
 	lags->t03 = (temp & PERIPHERAL_MASK_THIRD) >> 12;
-	temp = peripheral_read_upper(dev->base);
+	temp = peripheral_read_upper(dev);
 	lags->t12 = (temp & PERIPHERAL_MASK_FIRST);
 	lags->t13 = (temp & PERIPHERAL_MASK_SECOND) >> 6;
 	lags->t23 = (temp & PERIPHERAL_MASK_THIRD) >> 12;
