4e2240c5d0b92e8092a7259f92578119616581ff
Daniel Vorhaug <dvorhaug@gmail.com>
2023-04-27 15:23:04 +0200

    Angle runs

--- software/Turret/angles/angles.h
+++ software/Turret/angles/angles.h
@@ -6,7 +6,7 @@
 
 void transpose_matrix(int row, int column, double **matrix, double **transpose_matrix);
 void print_nxnmatrix(double **mat, int n);
-void destroy_matrix(double **arr);
+void destroy_matrix(double** arr, int low, int high);
 void print_matrix(double **matrix, int m, int n);
 void matrix_mult(double **mat_a, double **mat_b, double **result, int m, int n, int p);
 void matrix_mult_test(double **mat_a, double **mat_b, double **result, int m, int n, int n_2);
@@ -14,8 +14,9 @@ void create_r(double **r);
 void retireve_part_of_matrix(double **matrix, double **new, int row_m, int column_n);
 void calculate_x(double **V, double **U, double **Sigma, double **delays, double **result);
 double rad2deg(double radians);
-double theta(double **r);
+void dmatrixFill(double **m, int nrl, int nrh, int ncl, int nch);
+double theta(double **r, double** new_r);
 double phi(double **r);
-void get_angles_from_correlation(peripheral_lags lags, int *angles);
+void get_angles_from_correlation(peripheral_lags lags, int *angles, double** x, double** delays, double** v_t, double** u_t, double** sigma_inv, double** temp, double** temp2, double** new_r);
 
 #endif
--- software/turret/app.c
+++ software/turret/app.c
@@ -11,49 +11,59 @@
 #include "system.h"
 
 #define SLEEP_DURATION_US (25000) //  25  ms
-#define PANTILT_STEP_US   (25)    //  25  us
+#define PANTILT_STEP_US (25)	  //  25  us
 
-int main(void) {
-    // Hardware control structures
-    pantilt_dev pantilt = pantilt_inst((void *) PWM_0_BASE, (void *) PWM_1_BASE, (void *) PWM_2_BASE);
-	peripheral_dev peripheral = peripheral_inst((void *) PERIPHERAL_0_BASE);
+int main(void)
+{
+	// Hardware control structures
+	pantilt_dev pantilt = pantilt_inst((void *)PWM_0_BASE, (void *)PWM_1_BASE, (void *)PWM_2_BASE);
+	peripheral_dev peripheral = peripheral_inst((void *)PERIPHERAL_0_BASE);
 
-    // Initialize pwm hardware
-    pantilt_init(&pantilt);
+	// Initialize pwm hardware
+	pantilt_init(&pantilt);
 
-    // Create lags structure
-    peripheral_lags lags;
-    //lags = (peripheral_lags *)malloc(sizeof(peripheral_lags));
+	// Create lags structure
+	peripheral_lags lags;
+	// lags = (peripheral_lags *)malloc(sizeof(peripheral_lags));
 
-    // More setup probably
-    printf("Init complete, starting up...\n\n");
+	// More setup probably
+	printf("Init complete, starting up...\n\n");
 
-    // Init angles and matrices
-    double** u_t = dmatrix(1,3,1,6);
-    double** v_t = dmatrix(1,3,1,3);
-    double** sigma_inv = dmatrix(1,3,1,3);
-    int* angles = malloc(2*sizeof(int));
-    init_angles(u_t, v_t, sigma_inv);
+	// Init angles and matrices
+	double **x = dmatrix(1, 3, 1, 1);
+	double **delays = dmatrix(1, 6, 1, 1);
+	double **u_t = dmatrix(1, 3, 1, 6);
+	double **v_t = dmatrix(1, 3, 1, 3);
+	double **sigma_inv = dmatrix(1, 3, 1, 3);
+	double** temp = dmatrix(1,3,1,3);
+    double** temp2 = dmatrix(1,3,1,6);
+	double** new_r = dmatrix(1,3,1,1);
+	int *angles = malloc(2 * sizeof(int));
+	init_angles(u_t, v_t, sigma_inv);
 
+	printf("a");
 
-    // Main flow
-   while(true) {
+	// Main flow
+	while (true)
+	{
 
-    	// Read lags
-    	getLags(&peripheral, &lags);
+		// Read lags
+		getLags(&peripheral, &lags);
 		uint32_t debug = peripheral_read_debug(&peripheral);
-		
-        // Calculate angles
-        get_angles_from_correlation(lags, angles);
 
-        // Control PWM
+		// Calculate angles
+		get_angles_from_correlation(lags, angles, x, delays, v_t, u_t, sigma_inv, temp, temp2, new_r);
+
+		// Control PWM
 
 		// For debug
-    	printf("Lags: %d, %d, %d, %d, %d, %d\n", (int)lags.t01, (int)lags.t02, (int)lags.t03, (int)lags.t12, (int)lags.t13, (int)lags.t23);
-    	printf("vertical: %d, horizontal: %d", angles[0], angles[1]);
+		printf("Lags: %d, %d, %d, %d, %d, %d\n", (int)lags.t01, (int)lags.t02, (int)lags.t03, (int)lags.t12, (int)lags.t13, (int)lags.t23);
+		printf("vertical: %d, horizontal: %d", angles[0], angles[1]);
+		angles[0] = (angles[0] == 0x7fffffff) ? 0 : angles[0];
 		printf("debug: %d\n", (int)debug);
+		
 		usleep(1000000);
 	}
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
